22a1a3a3f497e50d55c2125b7675dec1cab6121e
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-09 22:08:11 -0500

    Add files via upload

---
+++ am.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=1,r,n;
+	cout<<"enter any number";
+	cin>>n;
+	while(i<=10)
+	{
+		r=n*i;
+		i++;
+		cout<<r<<"\n";
+	}
+	return 0;
+}
