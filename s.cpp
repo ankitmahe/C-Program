ee3da78b9003d5eab498b18d2d5c89e8c1d9e129
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-12 16:29:55 +0530

    Add files via upload

---
+++ s.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int p,r,t,si;
+    cout<<"enter a principal =";
+    cin>>p;
+    cout<<"enter a rate =";
+    cin>>r;
+    cout<<"enter a time =";
+    cin>>t;
+    si=p*r*t/100;
+    cout<<"Imple interest = "<<si;
+    return 0;
+}
