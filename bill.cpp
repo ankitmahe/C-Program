7127f7697f4f113c48241f4ec705eb8c5ea6b846
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-12 16:44:37 +0530

    Add files via upload

---
+++ bill.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int unit,perunit,bill;
+	cout<<"enter a unit = ";
+	cin>>unit;
+	cout<<"enter a perunit = ";
+	cin>>perunit;
+	bill=unit*perunit;
+	cout<<"Total Bill = "<<bill;
+	return 0;
+}
