b9044851c073376538ce5a0a88a44a1d3c8e29c2
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-14 16:23:40 +0530

    Add files via upload

---
+++ dac.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10,b=10;
+	float c=10.5,d=100.5;
+	cout<<++a<<endl;
+	cout<<--b<<endl;
+	cout<<++c<<endl;
+	cout<<--d;
+	return 0;
+}
