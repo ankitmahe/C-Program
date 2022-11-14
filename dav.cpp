2ae86c444ae7ac42abd8df1d80f02cb7840c3353
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-14 15:39:52 +0530

    Add files via upload

---
+++ dav.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,f;
+	cout<<"enter two number : ";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"sum of ="<<c<<endl;
+	d=a-b;
+	cout<<"subtraction of ="<<d<<endl;
+	e=a*b;
+	cout<<"multiplication of ="<<e<<endl;
+	f=a/b;
+	cout<<"divide of ="<<f;
+	return 0;
+}
