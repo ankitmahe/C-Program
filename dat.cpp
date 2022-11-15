1ba5d581a8444f3db4dcc51fefb6bb949bbb479f
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-15 15:23:01 +0530

    Add files via upload

---
+++ dat.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,c;
+	c=a;
+	cout<<c<<endl;
+	c+=a;
+	cout<<c<<endl;
+	c-=a;
+	cout<<c<<endl;
+	c*=a;
+	cout<<c<<endl;
+	c/=a;
+	cout<<c<<endl;
+	c%=a;
+	cout<<c;
+	return 0;
+}
