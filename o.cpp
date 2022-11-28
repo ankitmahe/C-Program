4d552b13e8da2c84bbc3b0bf73621a21b6cdda95
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2022-11-28 15:37:15 +0530

    Add files via upload

---
+++ o.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+    cout<<"Odd numbers between 1 to 10:\n";
+    while(i<10) 
+  {
+      i++; 
+      if(i%2==0){
+	  
+	  continue;
+
+      }
+      cout<<i<<" ";
+}
+  return 0;
+}
