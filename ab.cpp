a8ff4ea3e1d587d30db192835c0506e91a51ebf0
ankitmahe <117819682+ankitmahe@users.noreply.github.com>
2023-03-30 08:01:22 +0530

    Update ab.cpp

--- ab.cpp
+++ ab.cpp
@@ -5,13 +5,25 @@ int main()
 	int year;
 	cout<<"enter a year : ";
 	cin>>year;
-	if(year%4==0)
+	if(year%100==0)
 	{
+		if(year%400==0)
+		{
 		cout<<"It is Leapyear";
+		}
+		else
+		{
+			cout<<"It is not Leapyear";
+		}
 	}
-	else
+	else if (year%4==0)
 	{
 		cout<<"It is not Leapyear";
 	}
+	else
+		{
+			cout<<"It is not Leapyear";
+		}
+	
 	return 0;
 }
